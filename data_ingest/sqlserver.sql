74d929e7b0dcf9db719036c1b8e5f0b7330b5809
schowStriim <111879867+schowStriim@users.noreply.github.com>
2022-12-02 08:11:31 -0600

    Update sqlserver.sql

--- data_ingest/sqlserver.sql
+++ data_ingest/sqlserver.sql
@@ -3,6 +3,6 @@ Set @Id = 1
 
 While @Id <= 100000
 Begin 
-   Insert Into master.dms_sample.employee values (1, 'Albert - ' + CAST(@Id as nvarchar(10)),100)
+   Insert Into employee values (1, 'Albert - ' + CAST(@Id as nvarchar(10)),100)
    Set @Id = @Id + 1
 End
