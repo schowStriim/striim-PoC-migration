95a9bac9a21426a07d90677056b870a5d6dc2504
schowStriim <111879867+schowStriim@users.noreply.github.com>
2022-12-12 09:28:41 -0400

    Update postgres_delete_data.sql

--- cdc_data/postgres_delete_data.sql
+++ cdc_data/postgres_delete_data.sql
@@ -1,7 +1,2 @@
-WITH salary_list AS (
-    SELECT '{1000, 2000, 5000}'::INT[] salary
-)
-INSERT INTO striim_schema.employee
-(id, name_target, salary)
-SELECT n, 'Employee ' || n as name_target, salary[1 + mod(n, array_length(salary, 1))]
-FROM salary_list, generate_series(150001, 250000) as n;
+DELETE FROM striim_schema.employee 
+WHERE salary = 8000;
